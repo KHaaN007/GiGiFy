93222f260d11dd9360bf810e7bfdb588d8233cf5
KHaaN007 <khaanmr007@gmail.com>
2023-11-08 07:37:30 +0600

    Title Route Set

--- src/Root/RootLayout.jsx
+++ src/Root/RootLayout.jsx
@@ -1,8 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from '../component/NavBar/NavBar';
 import Footer from '../component/Footer/Footer';
+import { useEffect } from 'react';
 
 const RootLayout = () => {
+
+const location =useLocation()
+
+useEffect(()=>{
+    if(location.pathname === '/'){
+        document.title =`GiGiFy || Home`
+    }
+    else{
+        document.title= `GiGiFy ${location.pathname.replace('/', ' || ')}`
+    }
+},[location.pathname])
+
     return (
         <div>
             <NavBar></NavBar>
